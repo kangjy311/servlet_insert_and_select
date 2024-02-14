55e027c30229a3811fe58bc0facad08b59477993
kangjy311 <kangjy311@naver.com>
2024-02-14 18:15:51 +0900

    insert, select  DB 연결

---
+++ src/main/java/com/study/insert_and_select/entity/Student.java
@@ -0,0 +1,13 @@
+package com.study.insert_and_select.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+
+public class Student {
+	private int studentId;
+	private String name;
+	private int age;
+}
